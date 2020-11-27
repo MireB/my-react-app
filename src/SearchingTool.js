a5ad3618affdc46d61376bd893df645ff07f15ef
mirebosch <boschmire@gmail.com>
2020-11-27 16:19:22 +0100

    max width

--- src/SearchingTool.js
+++ src/SearchingTool.js
@@ -10,7 +10,7 @@ function SearchingTool() {
           type="search"
           className="form-control shadow-sm"
           placeholder="Enter a city"
-          size="20"
+          size="13"
         />
         <button type="submit" className="btn btn-primary mb-2 shadow-sm">
           Search
